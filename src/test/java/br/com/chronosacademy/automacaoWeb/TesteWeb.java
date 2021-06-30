e27ec3a8798690f34a3316ec6b55289f24a1e350
claytonchaves2014 <clayton.chaves@hotmail.com>
2021-06-30 20:15:29 -0300

    firt commit

--- src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
@@ -2,8 +2,12 @@ package br.com.chronosacademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
 
 public class TesteWeb {
     @Test
@@ -14,6 +18,9 @@ public class TesteWeb {
         driver.manage().window().maximize();
         driver.get("https://www.chronosacademy.com.br");
         String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
-        
+        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
+        String titulo = txtTitulo.getText();
+
+        assertEquals("Porque Tempo É Conhecimento", titulo);
     }
 }
